9ee368de7e041835373cec0889d5586c44042ab5
Boehl,Lukas <lukasboehl@web.de>
2022-08-25 20:31:33 +0200

    Visualisierungsfenster mit Radiobuttons "verschönert"
    Analysebericht erstellen Button eingefügt - es wird eine Tabelle erstellt, Spalten und Zeileneinträge werden automatisch befüllt, Kriterien für Zeileneinträge sind als vector in der Visualisierungsklasse hinterlegt

--- MAP/include/Visualization.hpp
+++ MAP/include/Visualization.hpp
@@ -3,18 +3,26 @@
 #include "CSV.hpp"
 #include "Column.hpp"
 #include <string>
+#include <vector>
 
 class Visualization {
 private:
+  std::vector<std::string> ColumnCriteria = {"Mittelwert", "Summe",
+                                             "Standardabweichung"};
+  std::string createAnalysisTableString(CSV csv);
+
 public:
   Visualization();
   std::string xColumn;
   std::string yColumn;
+  std::string
+  setXColumnOptions(CSV csv, std::string loaded_file,
+                    int chosen_columns[2]); // diese Methoden dürfen nicht
+                                            // private sein. Warum nicht?
+  std::string setYColumnOptions(CSV csv, std::string loaded_file,
+                                int chosen_columns[2]);
+  std::string showAnalysis(CSV csv, std::string chosen_file);
   void save(std::string path);
   void compute(std::string DB_DIR_Input, std::string chosen_file,
                int chosen_columns[2]);
-  std::string setXColumnOptions(CSV csv, std::string loaded_file,
-                                int chosen_columns[2]);
-  std::string setYColumnOptions(CSV csv, std::string loaded_file,
-                                int chosen_columns[2]);
 };
