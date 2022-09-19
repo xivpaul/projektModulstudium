8f5ceef6a9cd88ccaf95bdcefd977e530363da59
Boehl,Lukas <lukasboehl@web.de>
2022-09-19 16:33:48 +0200

    Neues Layout für Analyseberichte

--- MAP/src/Visualization.cpp
+++ MAP/src/Visualization.cpp
@@ -83,46 +83,71 @@ std::string Visualization::showAnalysis(CSV *csv, std::string chosen_file) {
   strftime(buffer, sizeof(buffer), "vom %d.%m.%Y, %H:%M:%SUhr", timeinfo);
   std::string timestamp(buffer);
   std::string anzahl_messpunkte = std::to_string(csv->columns[0].values.size());
-  std::string httpAnalysisReportString = "<head>\
+  std::string httpAnalysisReportString = "<!DOCTYPE html>\
+<html lang=\"en\">\
+ <meta charset=\"utf-8\" />\
+  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\
+<head>\
       <title>Analysebericht von " + chosen_file +
                                          "\
       </title>\
                                     <style>\
+      #outer_frame {\
+      margin-right: auto;\
+      margin-left: auto;\
+      width: 1000px;\
+    }\
+      #frame {\
+      margin-bottom: 0px;\
+      background: #ffffff;\
+      border-radius: .5em;\
+      padding: 2em;\
+      border: 1px solid black;\
+    }\
+      #plot_frame {\
+      background: #ffffff;\
+      border-radius: .5em;\
+      padding: 2em;\
+      border: 1px solid black;\
+    }\
 table, th, td {\
   border:1px solid black;\
 }\
 </style>\
 </head>\
 <body>\
-<br><div id = 'id_analysisreport'></ div></ body>\
-<h1> Analysebericht der Messdatei \"" + chosen_file +
-                                         "\"</h1>\
-                                         <h3>" +
+<div id='outer_frame'>\
+  <div id = 'frame'>\
+    <h1 style=\"margin-top:0px;\"> Analysebericht der Messdatei \"" +
+                                         chosen_file + "\"</h1><h3>" +
                                          timestamp + "</h3>\
-                                         <br>\
-<h3>Anzahl der Messpunkte: " + anzahl_messpunkte +
-                                         "</h3>\
-                                         <h2> Transformationshistorie</ h2>\
-<form><table> " + createTransformationHistoryTableString() +
+    <h4>Anzahl der Messpunkte: " + anzahl_messpunkte +
+                                         "</h4>\
+  </div>\
+  <div id='frame'>\
+    <h3 style=\"margin-top:-5px;\"> Transformationshistorie</h3>\
+    <table> " + createTransformationHistoryTableString() +
                                          "</table>\
-</form>\
-<h2> Uebersicht zur Messdatenbewertung</ h2>\
-                                    <table>\
-" + createAnalysisTableString(csv) + "\
-</table>" + createAnalysisReportPlot(csv) +
-                                         "\
-  <br>\
-  <br>\
-  <br>\
+  </div>\
+  <div id ='frame'>\
+    <h3 style=\"margin-top:-5px;\"> Übersicht zur Messdatenbewertung</h3>\
+    <table> " + createAnalysisTableString(csv) +
+                                         " </table >\
+  </div>\
+  <div id='frame'><h3 style=\"margin-top:-5px;margin-bottom:0px;\"> Visualisierung</h3>" +
+                                         createAnalysisReportPlot(csv) +
+                                         "</div>\
+                                         <br>\
 <form style=\"display: inline;\"action = \"http://localhost:8000\">\
-<input type = \"submit\" value = \"Zurueck zur Startseite\"/>\
+<input type = \"submit\" value = \"Zurück zur Startseite\"/>\
 </form>\
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
 <button onclick=\"printFunction();\">Drucken</button>\
 <script> function printFunction() {window.print();}\
 </script>\
-</ form> ";
-
+</div>\
+</ body>";
   return httpAnalysisReportString;
 }
 
@@ -193,12 +218,11 @@ std::string Visualization::createAnalysisReportPlot(CSV *csv) {
 
   data += "];";
   std::string visualizationHttp =
-      "<head><script src='plotly-2.12.1.min.js'></script> \
-                                    </head><body><div id='myDiv'></div></body> \
+      "<div id ='myDiv' style=\"margin-top: 0px;\"><script src='plotly-2.12.1.min.js'></script> \
                                     <script>" +
       traces + data + "\
                                         var layout = {\
-                                        width: 1000, \
+                                        width: 900,\
                                         showlegend: true,\
                                         autosize: false, \
                                          xaxis: {\
@@ -207,7 +231,7 @@ std::string Visualization::createAnalysisReportPlot(CSV *csv) {
                                           title: 'Messwert'}\
                                         }; \
                                         Plotly.newPlot('myDiv', data,layout); \
-                                    </script>";
+                                    </script></div>";
 
   return visualizationHttp;
 }
@@ -215,6 +239,3 @@ std::string Visualization::createAnalysisReportPlot(CSV *csv) {
 // Quelle Table erstellen: https://www.w3schools.com/html/html_tables.asp
 
 // Quelle für Print/speichern https://www.csestack.org/code-print-save-html-pdf/
-
-// &lt;!--<form style=\"display: inline;\">\
-// <a href = \"data:application/xml;charset=utf-8,your code here\" download=\"filename.txt\" >Als Textdatei speichern</a>--&gt\;
